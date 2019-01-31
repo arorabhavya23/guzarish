5867960a56573e612bfa37f3fe6324b18e95f12f
arorabhavya23 <arorabhavya23@gmail.com>
2019-01-31 22:41:06 +0530

    Updates

---
+++ precache-manifest.ac1cfe32a025c3a21d3f4e6eb655aa1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bc6b4a18152df93f0ff",
+    "url": "/guzarish/static/js/runtime~main.4bc6b4a1.js"
+  },
+  {
+    "revision": "90eb9779c0c00ba84483",
+    "url": "/guzarish/static/js/main.90eb9779.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/guzarish/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "90eb9779c0c00ba84483",
+    "url": "/guzarish/static/css/main.44669ecb.chunk.css"
+  },
+  {
+    "revision": "2b14e1d0f199475bde3d4dea35bb45e4",
+    "url": "/guzarish/index.html"
+  }
+];
